c231f5329b09271e900722d167572858ef49f6e0
Miguel Adán Vargas López <46058983+AdanVlpz7@users.noreply.github.com>
2022-05-12 17:33:51 -0500

    Inicio configuracion textArea y botones

--- MC68HC11Compiler/src/Compilador.java
+++ MC68HC11Compiler/src/Compilador.java
@@ -1,21 +1,94 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import compilerTools.CodeBlock;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import compilerTools.Directory;
+import compilerTools.ErrorLSSL;
+import compilerTools.Functions;
+import compilerTools.Grammar;
+import compilerTools.Production;
+import compilerTools.TextColor;
+import compilerTools.Token;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
 /**
  *
  * @author adan_
  */
 public class Compilador extends javax.swing.JFrame {
+    private String title; // titulo del compilador
+    private Directory Directorio; 
+    private ArrayList<Token> tokens; //lista donde se guardaran los tokens
+    private ArrayList<ErrorLSSL> errores; //lista para guardar todo tipo de errores posobles
+    private ArrayList<TextColor> textsColor; //lista de colores para palabras reservadas
+    private Timer timerKeyReleased; //para ejecutar una función que coloreara las palabras
+    private ArrayList<Production> identProd; //para extraer los identificadores en el analisis sintatico
+    private HashMap<String, String> identificadores; //donde guardaremos los identificadores
+    private boolean codeHasBeenCompiled = false;
 
-    /**
-     * Creates new form Compilador
-     */
     public Compilador() {
         initComponents();
+        init();
     }
-
+    
+    private void init(){
+        title = "Compilador para MC68HC11";
+        setLocationRelativeTo(null); //para centrar ventana
+        setTitle(title);
+        Directorio = new Directory(this, jtpCode, title, ".asd");
+        addWindowListener(new WindowAdapter(){
+            @Override
+            //para preguntar si se quiere guardar el codigo
+            public void windowClosing(WindowEvent e){
+                Directorio.Exit();
+                System.exit(0);
+            }
+        });
+        Functions.setLineNumberOnJTextComponent(jtpCode); //para que se numeren las lineas del codigo
+        timerKeyReleased = new Timer(300,((e) -> {
+            timerKeyReleased.stop();
+            colorAnalysis(); // se colorean el texto cada 300 milisegundos
+        }));
+        Functions.insertAsteriskInName(this, jtpCode, ()->{
+            timerKeyReleased.restart(); //para poner un asteristico cuando se edite
+        });
+        tokens = new ArrayList<>();
+        errores = new ArrayList<>();
+        textsColor = new ArrayList<>();
+        identProd = new ArrayList<>();
+   
+        identificadores = new HashMap<>();
+        //autocompletar algunas palabras
+        //funciona con CTRL+SPACE
+        Functions.setAutocompleterJTextComponent(new String[]{"OMG","HOLA","NATI","hola","magico","nati","comp"}, jtpCode, ()->{
+            timerKeyReleased.restart();
+        });
+        
+    }
+    
+    private void colorAnalysis(){
+        
+    }
+    private void clearFields(){
+        
+    }
+    
+    private void compile(){
+        
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -25,21 +98,207 @@ public class Compilador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnNuevo = new javax.swing.JButton();
+        btnAbrir = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtpCode = new javax.swing.JTextArea();
+        btnGuardar = new javax.swing.JButton();
+        btnGuardarC = new javax.swing.JButton();
+        btnCompilar = new javax.swing.JButton();
+        btnEjecutar = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnNuevo.setText("Nuevo");
+        btnNuevo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNuevoActionPerformed(evt);
+            }
+        });
+
+        btnAbrir.setText("Abrir");
+        btnAbrir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAbrirActionPerformed(evt);
+            }
+        });
+
+        jtpCode.setColumns(20);
+        jtpCode.setRows(5);
+        jScrollPane1.setViewportView(jtpCode);
+
+        btnGuardar.setText("Guardar");
+        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarActionPerformed(evt);
+            }
+        });
+
+        btnGuardarC.setText("Guardar como");
+        btnGuardarC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarCActionPerformed(evt);
+            }
+        });
+
+        btnCompilar.setText("Compilar");
+        btnCompilar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCompilarActionPerformed(evt);
+            }
+        });
+
+        btnEjecutar.setText("Ejecutar");
+        btnEjecutar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEjecutarActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Componente lexico", "Lexema", "[linea,columna]"
+            }
+        ));
+        jScrollPane3.setViewportView(jTable1);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane2.setViewportView(jTextArea1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnNuevo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnAbrir)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnGuardar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnGuardarC)
+                        .addGap(127, 127, 127)
+                        .addComponent(btnCompilar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnEjecutar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 452, Short.MAX_VALUE)
+                            .addComponent(jScrollPane2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(27, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnNuevo)
+                    .addComponent(btnAbrir)
+                    .addComponent(btnGuardar)
+                    .addComponent(btnGuardarC)
+                    .addComponent(btnCompilar)
+                    .addComponent(btnEjecutar))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNuevoActionPerformed
+        Directorio.New();
+        clearFields();
+    }//GEN-LAST:event_btnNuevoActionPerformed
+
+    private void btnGuardarCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarCActionPerformed
+        if(Directorio.SaveAs()){
+            colorAnalysis();
+            clearFields();
+        }
+    }//GEN-LAST:event_btnGuardarCActionPerformed
+
+    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+        if(Directorio.Save()){
+            colorAnalysis();
+            clearFields();
+        }
+        
+    }//GEN-LAST:event_btnGuardarActionPerformed
+
+    private void btnAbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAbrirActionPerformed
+        if(Directorio.Open()){
+            colorAnalysis();
+            clearFields();
+        }
+    }//GEN-LAST:event_btnAbrirActionPerformed
+
+    private void btnCompilarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCompilarActionPerformed
+        //si todavia no se ha guardado el codigo
+        if(getTitle().contains("*") || getTitle().equals(title)){
+            if(Directorio.Save()){
+                compile();
+            }
+        }
+        else{
+            compile();
+        }
+    }//GEN-LAST:event_btnCompilarActionPerformed
+
+    private void btnEjecutarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEjecutarActionPerformed
+        btnCompilar.doClick();
+        if(codeHasBeenCompiled){
+            if(!errores.isEmpty()){
+                JOptionPane.showMessageDialog(null, "no se puede ejecutar el codigo, ya que se encontraron errores");
+            }
+            else{
+                
+            }
+        }
+    }//GEN-LAST:event_btnEjecutarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -77,5 +336,17 @@ public class Compilador extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAbrir;
+    private javax.swing.JButton btnCompilar;
+    private javax.swing.JButton btnEjecutar;
+    private javax.swing.JButton btnGuardar;
+    private javax.swing.JButton btnGuardarC;
+    private javax.swing.JButton btnNuevo;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jtpCode;
     // End of variables declaration//GEN-END:variables
 }
